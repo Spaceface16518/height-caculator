eed331808fb3903cfcd4d6965075379d0e2868d7
Spaceface16518 <amrit.rathie@gmail.com>
2022-07-07 19:19:12 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#![deny(clippy::pedantic)]
+use std::{
+    thread,
+    time::{Duration, Instant},
+};
+
+use dialoguer::Input;
+use indicatif::{ProgressBar, ProgressStyle};
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    println!(
+        "--- Height Calculator tool --- v{} --- ",
+        env!("CARGO_PKG_VERSION")
+    );
+
+    let height: u32 = Input::new()
+        .with_prompt("Input your height")
+        .interact_text()?;
+
+    let pb = ProgressBar::new(2000).with_prefix("Calculating...");
+    pb.set_style(ProgressStyle::default_bar().template("{prefix} {wide_bar}"));
+    let start = Instant::now();
+    while start.elapsed() < Duration::from_secs(2) {
+        pb.inc(1);
+        thread::sleep(Duration::from_millis(1));
+    }
+    pb.finish_and_clear();
+
+    println!("Your height is {height}cm!");
+
+    Ok(())
+}
